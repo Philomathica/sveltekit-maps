d55b89d8d4b781b76ca2a9bfc2a19651fe6417fa
joris compernol <joriscompernol@gmail.com>
2022-01-04 09:33:12 +0100

    feat(shared): back to number[][]

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -31,7 +31,7 @@ export function setGeoRefData(width: number, height: number, sw: mapbox.LngLatLi
   };
 }
 
-export function getPositionInfo(georefData: GeoRefData): mapbox.LngLatLike[] {
+export function getPositionInfo(georefData: GeoRefData): number[][] {
   return [
     latLngToLngLat(projectPointForGeoreference([georefData.bbox[0], georefData.bbox[3]], georefData)),
     latLngToLngLat(projectPointForGeoreference([georefData.bbox[2], georefData.bbox[3]], georefData)),
@@ -87,7 +87,7 @@ export function getBoundingboxFeatures(points: mapbox.LngLatLike[]): GeoJSON.Fea
   };
 }
 
-export function getMarkersPosInfo(markerBL: mapbox.Marker, markerTR: mapbox.Marker, georefData: GeoRefData): mapbox.LngLatLike[] {
+export function getMarkersPosInfo(markerBL: mapbox.Marker, markerTR: mapbox.Marker, georefData: GeoRefData): number[][] {
   const { lng: lngBL, lat: latBL } = markerBL.getLngLat();
   const { lng: lngTR, lat: latTR } = markerTR.getLngLat();
   const newGeoRefData = {
