e855a69aedf6484629c6141ddc7a7a898ec1713b
joris compernol <joriscompernol@gmail.com>
2022-01-31 11:13:32 +0100

    feat(shared): add todos route

---
+++ src/routes/todos/index.svelte
@@ -0,0 +1,13 @@
+<div class="p-4">
+  <h1 class="uppercase font-bold my-2">Big</h1>
+  <ul class="list-disc list-inside">
+    <li>render points on /places</li>
+    <li>/home => selecting venues an zoom into building and show places</li>
+  </ul>
+
+  <h1 class="uppercase font-bold my-2">Small</h1>
+  <ul class="list-disc list-inside">
+    <li>delete on all edit-views</li>
+    <li>Cancel buttons on all edit views</li>
+  </ul>
+</div>
